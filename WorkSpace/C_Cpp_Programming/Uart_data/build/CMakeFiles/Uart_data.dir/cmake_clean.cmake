fdaee8026e51af3b0c9c38eb7d83dc3425a60375
A-s-a-d <asad.19.08.02@gmail.com>
2025-01-27 11:54:31 +0100

    added program that sends data to uart0, and tested

---
+++ WorkSpace/C_Cpp_Programming/Uart_data/build/CMakeFiles/Uart_data.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Uart_data.dir/src/main.cpp.o"
+  "CMakeFiles/Uart_data.dir/src/main.cpp.o.d"
+  "Uart_data"
+  "Uart_data.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Uart_data.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
