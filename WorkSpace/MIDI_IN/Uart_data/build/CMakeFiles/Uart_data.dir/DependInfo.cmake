0050953a92ffa65ba8237d569557032da7136dbe
Asad <asad.19.08.02@gmail.com>
2025-02-26 15:46:07 +0100

    UART recive midi data, we'll use 115200 baud cause midi one is not standard

---
+++ WorkSpace/MIDI_IN/Uart_data/build/CMakeFiles/Uart_data.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/admin/Desktop/Table-de-Mixage/WorkSpace/MIDI_IN/Uart_data/lib/Serial_port/Serial_port.cpp" "CMakeFiles/Uart_data.dir/lib/Serial_port/Serial_port.cpp.o" "gcc" "CMakeFiles/Uart_data.dir/lib/Serial_port/Serial_port.cpp.o.d"
+  "/home/admin/Desktop/Table-de-Mixage/WorkSpace/MIDI_IN/Uart_data/src/main.cpp" "CMakeFiles/Uart_data.dir/src/main.cpp.o" "gcc" "CMakeFiles/Uart_data.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ WorkSpace/MIDI_IN/Uart_data/build/CMakeFiles/Uart_data.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Uart_data.dir/lib/Serial_port/Serial_port.cpp.o"
+  "CMakeFiles/Uart_data.dir/lib/Serial_port/Serial_port.cpp.o.d"
+  "CMakeFiles/Uart_data.dir/src/main.cpp.o"
+  "CMakeFiles/Uart_data.dir/src/main.cpp.o.d"
+  "Uart_data"
+  "Uart_data.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Uart_data.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
